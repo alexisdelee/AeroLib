2d81c078fb68eb0927d5dad0bfe2ce52d9baf9cc
Alexis Delée <alexis.delee@hotmail.fr>
2017-05-12 00:43:36 +0200

    node server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const php = require("express-php");
+const errDomain = require("domain");
 
 const port = 8080;
 
@@ -8,7 +9,7 @@ function startServer() {
 	app.use(php.cgi("public"));
 	app.use(express.static("public"));
 
-	server = app.listen(port, "0.0.0.0", () => {
+	const server = app.listen(port, "0.0.0.0", () => {
 		console.log("port %d in %s mode", port, app.settings.env);
 	});
 }
@@ -20,9 +21,43 @@ if(require.main === module) {
 }
 
 app.use((request, response, next) => {
-	if(cluster.isWorker) {
-		console.log("Worker %d", cluster.worker.id);
-	}
+	let domain = errDomain.create();
+
+	domain.on("error", (error) => {
+		console.error("DOMAIN ERROR CAUGHT:", error.stack);
+
+		try {
+			setTimeout(() => {
+				console.error("Failsafe shutdown");
+
+				process.exit(1);
+			}, 5000);
+
+			let worker = cluster.worker;
+
+			if(worker) {
+				worker.disconnect();
+			}
+
+			server.close();
+
+			try {
+				next(error);
+			} catch(error) {
+				console.error("Failed to route Express error");
+				response.statusCode = 500;
+				response.setHeader("Content-Type", "text/plain");
+				response.send("Server error");
+			}
+		} catch(error) {
+			console.error("Unable to send 500 response\n", error.stack);
+		}
+	});
+
+	domain.add(request);
+	domain.add(response);
+
+	domain.run(next);
 	
-	return next();
+	// return next();
 });
--- server/serverCluster.js
+++ server/serverCluster.js
@@ -11,6 +11,13 @@ if(cluster.isMaster) {
 	});
 	
 	cluster.on("disconnect", (worker) => {
-		console.log("CLUSTER: Worker %d 
+		console.log("CLUSTER: Worker %d disconnected from the cluster", worker.id);
 	});
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log("CLUSTER: Worker %d died with exit code %d (%s)", worker.id, code, signal);
+    startWorker();
+  });
+} else {
+  require("./server.js")();
 }
